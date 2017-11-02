9e2eb3593abd60b815de2965937434731d1a406f
Megan Miller <meganmiller@Megans-MacBook-Pro.local>
2017-11-02 12:38:54 -0600

    Angular Reference

---
+++ js/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('friendsList').controller('mainCtrl', function($scope){
+    $scope.friends = ['Josie', 'Jared', 'Chase', 'Alan', 'Rob', 'Beth', 'Brock', 'Marina', 'Ryan', 'Jaden']
+})
